2ba930e58ff993b960079310b30834a0f813a373
Charles Butler <chuck@dasroot.net>
2013-07-07 01:43:10 -0400

    Adds ctrl p, pathogen, codeschool colorscheme, and symlink updates

---
+++ setup_symlinks.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln -s ~/.dotfiles/githelpers ~/.githelpers
+ln -s ~/.dotfiles/gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/zshrc  ~/.zshrc
+ln -s ~/.dotfiles/dotvim ~/.vim
+ln -s ~/.dotfiles/dotvimrc ~/.vimrc
